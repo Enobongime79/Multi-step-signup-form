3735248353f54446d3b7c044b6f7265c0173c540
Enobongime79 <enobongime79@gmail.com>
2026-02-17 17:04:01 +0100

    done the back and next button for the plan page

--- public/javascripts/infologic.js
+++ public/javascripts/infologic.js
@@ -91,3 +91,81 @@ toggleBtn.addEventListener("click", (event) => {
         proMonth.classList.remove("hidden");
     }
 })
+
+const toggleBtnMob = document.getElementById("toggleBtnMob");
+const monthlyMob = document.getElementById("monthlyMob");
+const yearlyMob = document.getElementById("yearlyMob");
+const arcadeMonthMob = document.getElementById("arcadeMonthMob");
+const arcadeYearMob = document.getElementById("arcadeYearMob");
+const advancedMonthMob = document.getElementById("advancedMonthMob");
+const advancedYearMob = document.getElementById("advancedYearMob");
+const proMonthMob = document.getElementById("proMonthMob");
+const proYearMob = document.getElementById("proYearMob");
+
+toggleBtnMob.addEventListener("click", (event) => {
+    if (event.target.checked) {
+        console.log("Checkbox is checked!")
+        yearlyMob.classList.add("text-[#15273F]")
+        yearlyMob.classList.remove("text-[#B9BABE]")
+        monthlyMob.classList.remove("text-[#15273F]");
+        monthlyMob.classList.add("text-[#B9BABE]");
+        arcadeYearMob.classList.remove("hidden");
+        arcadeMonthMob.classList.add("hidden");
+        advancedYearMob.classList.remove("hidden");
+        advancedMonthMob.classList.add("hidden");
+        proYearMob.classList.remove("hidden");
+        proMonthMob.classList.add("hidden");
+    }
+    else{
+        console.log("Checkbox is unchecked!")
+        monthlyMob.classList.add("text-[#15273F]")
+        monthlyMob.classList.remove("text-[#B9BABE]")
+        yearlyMob.classList.remove("text-[#15273F]");
+        yearlyMob.classList.add("text-[#B9BABE]");
+        arcadeYearMob.classList.add("hidden");
+        arcadeMonthMob.classList.remove("hidden");
+        advancedYearMob.classList.add("hidden");
+        advancedMonthMob.classList.remove("hidden");
+        proYearMob.classList.add("hidden");
+        proMonthMob.classList.remove("hidden");
+    }
+})
+
+const arcadeMob = document.getElementById("arcadeMob");
+const advancedMob = document.getElementById("advancedMob");
+const proMob = document.getElementById("proMob");
+
+
+
+arcadeMob.addEventListener("click", () => {
+    arcadeMob.classList.remove("border-[#D9D8DD]");
+    arcadeMob.classList.add("bg-[#EEF5FF]", "border-[#6661A1]");
+    advancedMob.classList.remove("bg-[#EEF5FF]", "border-[#6661A1]");
+    advancedMob.classList.add("border-[#D9D8DD]");
+    proMob.classList.add("border-[#D9D8DD]");
+    proMob.classList.remove("bg-[#EEF5FF]", "border-[#6661A1]");
+    plan = "arcade";
+    console.log(plan)
+})
+
+advancedMob.addEventListener("click", () => {
+    advancedMob.classList.remove("border-[#D9D8DD]");
+    arcadeMob.classList.remove("bg-[#EEF5FF]", "border-[#6661A1]");
+    advancedMob.classList.add("bg-[#EEF5FF]", "border-[#6661A1]");
+    arcadeMob.classList.add("border-[#D9D8DD]");
+    proMob.classList.add("border-[#D9D8DD]");
+    proMob.classList.remove("bg-[#EEF5FF]", "border-[#6661A1]");
+    plan = "advanced";
+    console.log(plan)
+})
+
+proMob.addEventListener("click", () => {
+    proMob.classList.remove("border-[#D9D8DD]");
+    arcadeMob.classList.remove("bg-[#EEF5FF]", "border-[#6661A1]");
+    advancedMob.classList.remove("bg-[#EEF5FF]", "border-[#6661A1]");
+    proMob.classList.add("bg-[#EEF5FF]", "border-[#6661A1]");
+    advancedMob.classList.add("border-[#D9D8DD]");
+    arcadeMob.classList.add("border-[#D9D8DD]");
+    plan = "pro";
+    console.log(plan)
+})
