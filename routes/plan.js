377876a9ad1ced75ca784e17f772ba95a950ed3f
Enobongime79 <enobongime79@gmail.com>
2026-02-17 17:12:34 +0100

    fixed a routing issue

--- routes/plan.js
+++ routes/plan.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('plan', { title: 'Plans' });
+  return res.redirect('/')
 });
 
 router.post("/", (req, res) => {
